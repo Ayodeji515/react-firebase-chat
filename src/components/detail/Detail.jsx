cb41ec4c9e2051a9ae58cfadd819ed38eb7891f3
LazyTechie <emmanuelayodeji515@gmail.com>
2024-08-05 22:28:03 -0700

    first commit

---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,92 @@
+import "./detail.css";
+
+
+
+const Detail = () => {
+
+  return (
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Ayo Deji</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg2.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg2.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg2.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./bg2.jpg" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+
+      </div>
+    </div>  
+  )
+}
+
+
+export default Detail
---
+++ src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,18 @@
+import "./userInfo.css";
+
+const Userinfo = () => {
+  return (
+    <div className="userInfo">
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>Hay Why</h2>
+        </div>
+        <div className="icons"></div>
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+    </div>
+  )
+}
+
+export default Userinfo
